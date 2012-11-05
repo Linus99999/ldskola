9646a5cb453fe26480ac03537bb1abf3805bfbc2
Linus <Linus.ostren@gmail.com>
2012-11-05 20:07:05 +0100

    mapp

--- auto.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-Sorted = open("output.txt", "r")
-Facit = open("facit.txt", "r")
-def compare():
-    return (Sorted.read() == Facit.read())
-
-    
-print(compare())
--- test.py
+++
@@ -1,8 +0,0 @@
-from random import *
-def add5(x):
-    return x+5
-
-print("hello world")
-
-print(add5(5))
-print(random())
