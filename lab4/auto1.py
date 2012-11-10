4c8ed4c6907a1b0406ab4317249dde835df008f3
Linus <Linus.ostren@gmail.com>
2012-11-10 21:28:07 +0100

    CPPPPPPPPPPPPPPPPPPPPPPPPPPPPP

---
+++ lab4/auto1.py
@@ -0,0 +1,361 @@
+import os
+import time
+def compare(x):
+    Sorted = open("output.txt", "rw")
+    Facit = open(x, "r")
+    return (Sorted.read() == Facit.read())
+#1 Alla samma
+#2 Stigande backar
+#3 Saknad front
+#4 Stigande
+#5 Fallande backar
+#6 Saknad mitt
+#7 Fallande
+#8 Orgel
+#9 Slumpvis
+passed = 0
+failed = 0
+x = time.time()
+os.system("./sort1 < 1.txt > output.txt")
+if (compare("f1.txt")):
+    passed = passed + 1
+else:
+    print ("sort1 failed 1.")
+    failed = failed + 1
+os.system("./sort1 < 2.txt > output.txt")
+if (compare("f2.txt")):
+    passed = passed + 1
+else:
+    print ("sort1 failed 2.")
+    failed = failed + 1
+os.system("./sort1 < 3.txt > output.txt")
+if (compare("f3.txt")):
+    passed = passed + 1
+else:
+    print ("sort1 failed 3.")
+    failed = failed + 1
+os.system("./sort1 < 4.txt > output.txt")
+if (compare("f4.txt")):
+    passed = passed + 1
+else:
+    print ("sort1 failed 4.")
+    failed = failed + 1
+os.system("./sort1 < 5.txt > output.txt")
+if (compare("f5.txt")):
+    passed = passed + 1
+else:
+    print ("sort1 failed 5.")
+    failed = failed + 1
+os.system("./sort1 < 6.txt > output.txt")
+if (compare("f6.txt")):
+    passed = passed + 1
+else:
+    print ("sort1 failed 6.")
+    failed = failed + 1
+os.system("./sort1 < 7.txt > output.txt")
+if (compare("f7.txt")):
+    passed = passed + 1
+else:
+    print ("sort1 failed 7.")
+    failed = failed + 1
+os.system("./sort1 < 8.txt > output.txt")
+if (compare("f8.txt")):
+    passed = passed + 1
+else:
+    print ("sort1 failed 8.")
+    failed = failed + 1
+os.system("./sort1 < 9.txt > output.txt")
+if (compare("f9.txt")):
+    passed = passed + 1
+else:
+    print ("sort1 failed 9.")
+    failed = failed + 1
+print(time.time()-x)
+#######
+x = time.time()
+os.system("./sort2 < 1.txt > output.txt")
+if (compare("f1.txt")):
+    passed = passed + 1
+else:
+    print ("sort2 failed 1.")
+    failed = failed + 1
+os.system("./sort2 < 2.txt > output.txt")
+if (compare("f2.txt")):
+    passed = passed + 1
+else:
+    print ("sort2 failed 2.")
+    failed = failed + 1
+os.system("./sort2 < 3.txt > output.txt")
+if (compare("f3.txt")):
+    passed = passed + 1
+else:
+    print ("sort2 failed 3.")
+    failed = failed + 1
+os.system("./sort2 < 4.txt > output.txt")
+if (compare("f4.txt")):
+    passed = passed + 1
+else:
+    print ("sort2 failed 4.")
+    failed = failed + 1
+os.system("./sort2 < 5.txt > output.txt")
+if (compare("f5.txt")):
+    passed = passed + 1
+else:
+    print ("sort2 failed 5.")
+    failed = failed + 1
+os.system("./sort2 < 6.txt > output.txt")
+if (compare("f6.txt")):
+    passed = passed + 1
+else:
+    print ("sort2 failed 6.")
+    failed = failed + 1
+os.system("./sort2 < 7.txt > output.txt")
+if (compare("f7.txt")):
+    passed = passed + 1
+else:
+    print ("sort2 failed 7.")
+    failed = failed + 1
+os.system("./sort2 < 8.txt > output.txt")
+if (compare("f8.txt")):
+    passed = passed + 1
+else:
+    print ("sort2 failed 8.")
+    failed = failed + 1
+os.system("./sort2 < 9.txt > output.txt")
+if (compare("f9.txt")):
+    passed = passed + 1
+else:
+    print ("sort2 failed 9.")
+    failed = failed + 1
+print(time.time()-x)
+#######
+x = time.time()
+os.system("./sort3 < 1.txt > output.txt")
+if (compare("f1.txt")):
+    passed = passed + 1
+else:
+    print ("sort3 failed 1.")
+    failed = failed + 1
+os.system("./sort3 < 2.txt > output.txt")
+if (compare("f2.txt")):
+    passed = passed + 1
+else:
+    print ("sort3 failed 2.")
+    failed = failed + 1
+os.system("./sort3 < 3.txt > output.txt")
+if (compare("f3.txt")):
+    passed = passed + 1
+else:
+    print ("sort3 failed 3.")
+    failed = failed + 1
+os.system("./sort3 < 4.txt > output.txt")
+if (compare("f4.txt")):
+    passed = passed + 1
+else:
+    print ("sort3 failed 4.")
+    failed = failed + 1
+os.system("./sort3 < 5.txt > output.txt")
+if (compare("f5.txt")):
+    passed = passed + 1
+else:
+    print ("sort3 failed 5.")
+    failed = failed + 1
+os.system("./sort3 < 6.txt > output.txt")
+if (compare("f6.txt")):
+    passed = passed + 1
+else:
+    print ("sort3 failed 6.")
+    failed = failed + 1
+os.system("./sort3 < 7.txt > output.txt")
+if (compare("f7.txt")):
+    passed = passed + 1
+else:
+    print ("sort3 failed 7.")
+    failed = failed + 1
+os.system("./sort3 < 8.txt > output.txt")
+if (compare("f8.txt")):
+    passed = passed + 1
+else:
+    print ("sort3 failed 8.")
+    failed = failed + 1
+os.system("./sort3 < 9.txt > output.txt")
+if (compare("f9.txt")):
+    passed = passed + 1
+else:
+    print ("sort3 failed 9.")
+    failed = failed + 1
+print(time.time()-x)
+#######
+x = time.time()
+os.system("./sort4 < 1.txt > output.txt")
+if (compare("f1.txt")):
+    passed = passed + 1
+else:
+    print ("sort4 failed 1.")
+    failed = failed + 1
+os.system("./sort4 < 2.txt > output.txt")
+if (compare("f2.txt")):
+    passed = passed + 1
+else:
+    print ("sort4 failed 2.")
+    failed = failed + 1
+os.system("./sort4 < 3.txt > output.txt")
+if (compare("f3.txt")):
+    passed = passed + 1
+else:
+    print ("sort4 failed 3.")
+    failed = failed + 1
+os.system("./sort4 < 4.txt > output.txt")
+if (compare("f4.txt")):
+    passed = passed + 1
+else:
+    print ("sort4 failed 4.")
+    failed = failed + 1
+os.system("./sort4 < 5.txt > output.txt")
+if (compare("f5.txt")):
+    passed = passed + 1
+else:
+    print ("sort4 failed 5.")
+    failed = failed + 1
+os.system("./sort4 < 6.txt > output.txt")
+if (compare("f6.txt")):
+    passed = passed + 1
+else:
+    print ("sort4 failed 6.")
+    failed = failed + 1
+os.system("./sort4 < 7.txt > output.txt")
+if (compare("f7.txt")):
+    passed = passed + 1
+else:
+    print ("sort4 failed 7.")
+    failed = failed + 1
+os.system("./sort4 < 8.txt > output.txt")
+if (compare("f8.txt")):
+    passed = passed + 1
+else:
+    print ("sort4 failed 8.")
+    failed = failed + 1
+os.system("./sort4 < 9.txt > output.txt")
+if (compare("f9.txt")):
+    passed = passed + 1
+else:
+    print ("sort4 failed 9.")
+    failed = failed + 1
+print(time.time()-x)
+#######
+x = time.time()
+os.system("./sort5 < 1.txt > output.txt")
+if (compare("f1.txt")):
+    passed = passed + 1
+else:
+    print ("sort5 failed 1.")
+    failed = failed + 1
+os.system("./sort5 < 2.txt > output.txt")
+if (compare("f2.txt")):
+    passed = passed + 1
+else:
+    print ("sort5 failed 2.")
+    failed = failed + 1
+os.system("./sort5 < 3.txt > output.txt")
+if (compare("f3.txt")):
+    passed = passed + 1
+else:
+    print ("sort5 failed 3.")
+    failed = failed + 1
+os.system("./sort5 < 4.txt > output.txt")
+if (compare("f4.txt")):
+    passed = passed + 1
+else:
+    print ("sort5 failed 4.")
+    failed = failed + 1
+os.system("./sort5 < 5.txt > output.txt")
+if (compare("f5.txt")):
+    passed = passed + 1
+else:
+    print ("sort5 failed 5.")
+    failed = failed + 1
+os.system("./sort5 < 6.txt > output.txt")
+if (compare("f6.txt")):
+    passed = passed + 1
+else:
+    print ("sort5 failed 6.")
+    failed = failed + 1
+os.system("./sort5 < 7.txt > output.txt")
+if (compare("f7.txt")):
+    passed = passed + 1
+else:
+    print ("sort5 failed 7.")
+    failed = failed + 1
+os.system("./sort5 < 8.txt > output.txt")
+if (compare("f8.txt")):
+    passed = passed + 1
+else:
+    print ("sort5 failed 8.")
+    failed = failed + 1
+os.system("./sort5 < 9.txt > output.txt")
+if (compare("f9.txt")):
+    passed = passed + 1
+else:
+    print ("sort5 failed 9.")
+    failed = failed + 1
+print(time.time()-x)
+#######
+x = time.time()
+os.system("./sort2 < 1.txt > output.txt")
+if (compare("f1.txt")):
+    passed = passed + 1
+else:
+    print ("sort6 failed 1.")
+    failed = failed + 1
+os.system("./sort6 < 2.txt > output.txt")
+if (compare("f2.txt")):
+    passed = passed + 1
+else:
+    print ("sort6 failed 2.")
+    failed = failed + 1
+os.system("./sort6 < 3.txt > output.txt")
+if (compare("f3.txt")):
+    passed = passed + 1
+else:
+    print ("sort6 failed 3.")
+    failed = failed + 1
+os.system("./sort6 < 4.txt > output.txt")
+if (compare("f4.txt")):
+    passed = passed + 1
+else:
+    print ("sort6 failed 4.")
+    failed = failed + 1
+os.system("./sort6 < 5.txt > output.txt")
+if (compare("f5.txt")):
+    passed = passed + 1
+else:
+    print ("sort6 failed 5.")
+    failed = failed + 1
+os.system("./sort6 < 6.txt > output.txt")
+if (compare("f6.txt")):
+    passed = passed + 1
+else:
+    print ("sort6 failed 6.")
+    failed = failed + 1
+os.system("./sort6 < 7.txt > output.txt")
+if (compare("f7.txt")):
+    passed = passed + 1
+else:
+    print ("sort6 failed 7.")
+    failed = failed + 1
+os.system("./sort6 < 8.txt > output.txt")
+if (compare("f8.txt")):
+    passed = passed + 1
+else:
+    print ("sort6 failed 8.")
+    failed = failed + 1
+os.system("./sort6 < 9.txt > output.txt")
+if (compare("f9.txt")):
+    passed = passed + 1
+else:
+    print ("sort6 failed 9.")
+    failed = failed + 1
+print(time.time()-x)
+
+print("Passed: %d\n"%passed)
+print("Failed: %d\n"%failed)
---
+++ lab4/makeinput1.py
@@ -0,0 +1,206 @@
+import random
+import sys
+intFrom = int(sys.argv[1]) 
+intTo = int(sys.argv[2])
+total = int(sys.argv[3])
+List=[]
+count = 0
+test = open("1.txt", "w")
+facit = open("f1.txt", "w")
+test.write("%s\n"%total)
+ran = random.randint(intFrom, intTo)
+while(count < total):
+    test.write("%s\n"%str(ran))
+    List.insert(count,ran)
+    count = count + 1
+    ran = random.randint(intFrom, intTo)
+List.sort()
+x = 0
+while(x < total):
+    facit.write("%s\n"%List[x])
+    x = x + 1
+
+test = open("2.txt", "w")
+facit = open("f2.txt", "w")
+
+listtemp = []
+test.write("%s\n"%total)
+
+while (count < ((total/2)+(total % 2))):
+    ran = random.randint(intFrom,intTo)
+    List.insert(count,ran)
+    count = count + 1
+count = 0
+while (count < (total/2)):
+    ran = random.randint(intFrom,intTo)
+    listtemp.insert(count,ran)
+    count = count + 1        
+List.sort()
+listtemp.sort()
+List = List + listtemp
+y = 0
+while(y < total):
+    test.write("%s\n"%List[y])
+    y = y + 1
+List.sort()
+x = 0
+while(x < total):
+    facit.write("%s\n"%List[x])
+    x = x + 1
+
+test = open("3.txt", "w")
+facit = open("f3.txt", "w")
+
+test.write("%s\n"%total)
+while (count < total):
+    ran = random.randint(intFrom,intTo)
+    List.insert(count,ran)
+    count = count + 1
+List.sort()
+listtemp = List
+beginning = List.pop(0)
+List.append(beginning)
+y = 0
+while(y < total):
+    test.write("%s\n"%List[y])
+    y = y + 1
+List.sort()
+x = 0
+while(x < total):
+    facit.write("%s\n"%List[x])
+    x = x + 1
+
+test = open("4.txt", "w")
+facit = open("f4.txt", "w")
+
+test.write("%s\n"%total)
+y = 0
+while(count < total):
+    ran = random.randint(intFrom,intTo)
+    List.insert(count,ran)
+    count = count + 1
+List.sort()
+count = 0
+while(count < total):
+    test.write("%s\n"%List[count])
+    count = count + 1
+List.sort()
+x = 0
+while(x < total):
+    facit.write("%s\n"%List[x])
+    x = x + 1
+
+test = open("5.txt", "w")
+facit = open("f5.txt", "w")
+
+listtemp = []
+test.write("%s\n"%total)
+while (count < ((total/2)+(total % 2))):
+    ran = random.randint(intFrom,intTo)
+    List.insert(count,ran)
+    count = count + 1
+count = 0
+while (count < (total/2)):
+    ran = random.randint(intFrom,intTo)
+    listtemp.insert(count,ran)
+    count = count + 1        
+List.sort()
+listtemp.sort()
+List = List + listtemp
+List.reverse()
+y = 0
+while(y < total):
+    test.write("%s\n"%List[y])
+    y = y + 1
+List.sort()
+x = 0
+while(x < total):
+    facit.write("%s\n"%List[x])
+    x = x + 1
+
+test = open("6.txt", "w")
+facit = open("f6.txt", "w")
+
+test.write("%s\n"%total)
+while(count < total):
+    ran = random.randint(intFrom,intTo)
+    List.insert(count,ran)
+    count = count + 1
+List.sort()
+mitten = total/2
+mitten = List.pop(mitten)
+List.append(mitten)
+count = 0
+while(count < total):
+    test.write("%s\n"%List[count])
+    count = count + 1
+List.sort()
+x = 0
+while(x < total):
+    facit.write("%s\n"%List[x])
+    x = x + 1
+
+test = open("7.txt", "w")
+facit = open("f7.txt", "w")
+
+test.write("%s\n"%total)
+y = 0
+while(count < total):
+    ran = random.randint(intFrom,intTo)
+    List.insert(count,ran)
+    count = count + 1
+List.sort()
+List.reverse()
+count = 0
+while(count < total):
+    test.write("%s\n"%List[count])
+    count = count + 1
+List.sort()
+x = 0
+while(x < total):
+    facit.write("%s\n"%List[x])
+    x = x + 1
+    
+test = open("8.txt", "w")
+facit = open("f8.txt", "w")
+
+listtemp = []
+test.write("%s\n"%total)
+
+while (count < ((total/2)+(total % 2))):
+    ran = random.randint(intFrom,intTo)
+    List.insert(count,ran)
+    count = count + 1
+count = 0
+while (count < (total/2)):
+    ran = random.randint(intFrom,intTo)
+    listtemp.insert(count,ran)
+    count = count + 1        
+List.sort()
+listtemp.sort()
+listtemp.reverse()
+List = List + listtemp
+y = 0
+while(y < total):
+    test.write("%s\n"%List[y])
+    y = y + 1
+List.sort()
+x = 0
+while(x < total):
+    facit.write("%s\n"%List[x])
+    x = x + 1
+    
+test = open("9.txt", "w")
+facit = open("f9.txt", "w")
+
+test.write("%s\n"%total)
+while (count < total):
+    ran = random.randint(intFrom,intTo)
+    test.write("%s\n"%str(ran))
+    List.insert(count,ran)
+    count = count + 1
+List.sort()
+x = 0
+while(x < total):
+    facit.write("%s\n"%List[x])
+    x = x + 1
