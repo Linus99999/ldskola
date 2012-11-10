41e7156e0df8189de38afe57ced3a01a8a19e8fd
David <david@ubuntu.(none)>
2012-11-10 17:39:21 +0100

    tog bort lab4.py

--- lab4.py
+++
@@ -1,9 +0,0 @@
-import os
-import random
-count = 0
-Out = open("test.txt", "w")
-x = 100000
-Out.write("%s\n"%x)
-while (count < x):
-    Out.write("%s\n"%str(random.randint(0,1000000)))
-    count = count + 1
