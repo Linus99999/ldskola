2eb861594f3ebdfc93793b00f6ea83ebed563794
David <david@ubuntu.(none)>
2012-11-10 17:36:31 +0100

    *.java och *.class

---
+++ lab5/MyDieTest.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class MyDieTest {
+    public static void main(string [] args) {
+	Scanner scan = new Scanner(System.in);
+	System.out.print("Number of sides: ");
+	Die Dice = new Die();
+	
+    }
+
+
+}
