06a9856a118507c4b33f035603003bf32ddaea46
David <david@ubuntu.(none)>
2012-11-19 18:38:47 +0100

    javadoc

--- inlupp4/src/Car.java
+++ inlupp4/src/Car.java
@@ -1,35 +1,52 @@
-
+/**
+ * 
+ * Represents a class of cars with a borntime and a destination variable. The destination variable represents the way the car is going to go.
+ * 
+ */
 public class Car {
 	
 	private int bornTime;
-	private int dest; // 1 för rakt fram, 2 för vänstersväng
-	
+	private int destination; // 1 fï¿½r rakt fram, 2 fï¿½r vï¿½nstersvï¿½ng
+/**
+ * Construct a car with borntime and default destination = 1.
+ * @param temp_borntime The time when the car is created.
+ */
 	public Car(int temp_borntime){
 		bornTime = temp_borntime;
-		dest = 1;
+		destination = 1;
 		
 	}
-
-	public Car(int temp_borntime, int temp_dest){
+/**
+ * Construct a car.
+ * @param temp_borntime The time when the car is created.
+ * @param temp_destination Decides which way the car is going, 1 for straight forward and 2 for a left turn.
+ */
+	public Car(int temp_borntime, int temp_destination){
 		bornTime = temp_borntime;
-		dest = temp_dest;
+		destination = temp_destination;
 		
 	}
-	
+/**
+ * 
+ * @return Returns an int of the cars borntime.
+ */
 	public int getbornTime(){
 		
 		return bornTime;
 	}
-	
-	public int getdest(){
+	/**
+	 * 
+	 * @return Returns an int of the cars destination variable.
+	 */
+	public int getdestination(){
 		
-		return dest;
+		return destination;
 	}
 	
-	
+
 	    
 	public String toString() {
-		return "Car: \nbornTime=" + this.bornTime + " \ndest =" + this.dest;
+		return "Car: \nbornTime=" + bornTime + " \ndest =" + destination;
 		
 		
 	}
@@ -39,8 +56,16 @@ public class Car {
 		Car b = new Car(2);
 		System.out.println(b);
 		System.out.println(b.getbornTime());
-		System.out.println(b.getdest());
+		System.out.println(b.getdestination());
 		
 	}
+
+}
+
+
+int max;
+if(time-borntime > max){
+	max = time - borntime;
+}
 
 }
