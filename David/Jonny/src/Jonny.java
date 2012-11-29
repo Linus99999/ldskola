8ddf79da6e13c968aa4721ace371b71d3a3d37e7
David <david.jansson@telia.com>
2012-11-29 11:50:59 +0100

    Jonny

--- David/Jonny/src/Jonny.java
+++ David/Jonny/src/Jonny.java
@@ -5,29 +5,54 @@ import java.awt.*;
     
     
 public class Jonny extends JApplet implements ActionListener{
-	  	private JLabel etiketten = new JLabel("Vad heter du?");
+	  	
+		int n = 1;
+		int x;
+	
+		private JLabel etiketten = new JLabel("Vad heter du?");
 	  	private JTextField inmatningsfältet = new JTextField(6);
 	  	
 	  	private JLabel utskriften = new JLabel();
 	  	private JButton knappen1 = new JButton("Kör!");
-	  	private JButton knappen2 = new JButton("Kör!");
-	  	private JButton knappen3 = new JButton("Kör!");
+	  	private JButton knappen2 = new JButton("Lägg till en knapp");
+	  	//private JButton knappen3 = new JButton("Kör!");
 	  	
 	  	
 	  public void init(){
+		  
 		  knappen1.addActionListener(this);
 		  knappen2.addActionListener(this);
-		  knappen3.addActionListener(this);
+		  System.out.println("1");
+		  //knappen3.addActionListener(this);
 		  Container cp = getContentPane();
 		  cp.setLayout(new FlowLayout());
 		  cp.add(etiketten);
 		  cp.add(inmatningsfältet);
 		  cp.add(knappen1);
 		  cp.add(knappen2);
-		  cp.add(knappen3);
+		  //cp.add(knappen3);
 		  cp.add(utskriften);
+		  System.out.println("2");
+		  
+		  while (n<=x){
+			  System.out.println("w1");
+			  JButton knappen4 = new JButton("Kör!");
+			  knappen4.addActionListener(this);
+			  cp.add(knappen4);
+			  n++;
+		  }
+		  
+		  
 		  
 	  }
+	  
+	  	/*public void createNewField() {
+		JButton knappen4 = new JButton("Ny!");
+		Container cp = getContentPane();
+		cp.add(knappen4);
+		System.out.println("c1");
+			
+	  }*/
 
 	  
 	  public void actionPerformed(ActionEvent event){
@@ -36,13 +61,19 @@ public class Jonny extends JApplet implements ActionListener{
 			  String namnet = inmatningsfältet.getText();
 			  utskriften.setText("Hej, " + namnet + "!");
 		  }else if(vilken == knappen2){
-			  utskriften.setText("Hej, knappen2!");
+			  //createNewField();
+			  x++;
+			  init();
+			  //utskriften.setText("Hej, knappen2!");
 		  }else {
-			  utskriften.setText("Va tusan?");
+			  
+			  
+			  //utskriften.setText("Va tusan?");
 		  }
 		  
 		  
 		  
 	  }
+	  
+	 
 }
-//Kanske
