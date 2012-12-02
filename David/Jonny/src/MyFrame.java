73ffca3ce7192d12d26e0d93703c1633f119d37a
David <david@ubuntu.(none)>
2012-12-02 14:44:26 +0100

    sa

--- David/Jonny/src/MyFrame.java
+++ David/Jonny/src/MyFrame.java
@@ -4,7 +4,7 @@ import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
-
+import java.util.Arrays;
 
 
 
@@ -12,6 +12,21 @@ import javax.swing.*;
 	
  class MyFrame extends JFrame implements ActionListener{
 	private GridBagConstraints c = new GridBagConstraints();
+	
+	int x;
+	int i;
+	int u = 0;
+	double finalresultat;
+	
+	JTextField[] textField1 = new JTextField[100];
+	JTextField[] textField2 = new JTextField[100];
+	
+	
+	
+	/*int[] prisarray = new int[100];
+	
+	String[] enhetsarray = new String[100];
+	*/
 	
 	public MyFrame(){
 		super();
@@ -21,6 +36,12 @@ import javax.swing.*;
 		c.gridx = 0;
 		c.gridy = 0;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		while (u<100){
+			textField1[u] = null;
+			textField2[u] = null;
+			u++;
+		}
 		addNewButton();
 	}
 				
@@ -50,25 +71,41 @@ import javax.swing.*;
 		pack();
 	}
 	private void addNewTextField(){
-		JTextField textField1 = new JTextField(6);
-		JTextField textField2 = new JTextField(6);
+		
+		textField1[i] = new JTextField(6);
+		textField1[i].setText("1");
 		c.gridx++;
-		add(textField1,c);
+		add(textField1[i],c);
 		c.gridx = c.gridx +2;
-		add(textField2,c);
+		
+		textField2[i] = new JTextField(6);
+		textField2[i].setText("1");
+		add(textField2[i],c);
 		c.gridx = c.gridx -3;
 		pack();
 		c.gridy++;
 	}
 	
-	private void addNewOutput(String s){
+	private void addNewOutput(Double s){
 		 JLabel utskrift = new JLabel();
 		 c.gridy = c.gridy +2;
 		 add(utskrift,c);
-		 utskrift.setText(s);
+		 
+		 utskrift.setText(s + "");
 		 pack();
 		 
 		 
+	}
+	
+	private double computeResult(){
+		int count = 0;
+		while (textField1[count] != null){
+		JTextField pris = textField1[count];
+		finalresultat += Double.parseDouble(pris.getText());
+		count++;
+	}
+		return finalresultat;
+		
 	}
 				
 	@Override
@@ -77,15 +114,21 @@ import javax.swing.*;
 			
 			addNewLabel();
 			addNewTextField();
+			
 		}else if(e.getActionCommand().equals("resultat")){
 		
-			addNewOutput("Resultat:");
+			if(x==0){
+				double resultat = computeResult();
+				addNewOutput(resultat);
+				x++;
+			} //computeResult();
 			
 	}
 	}
 	
 	
 	public static void main(String [] args){
+		
 		new MyFrame();
 	}
 				
