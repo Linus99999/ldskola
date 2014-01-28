ad8d5bb4f83748479d64f7b697690f4a85d3fd9b
Linus <Linus.ostren@gmail.com>
2014-01-28 15:35:16 +0100

    n

--- dbs/bookstore.erl
+++ dbs/bookstore.erl
@@ -1,5 +1,12 @@
+%%%-------------------------------------------------------------------
+%%% @author Linus Östrén, David Rönn Jansson
+%%%
+%%% Created : 28 Jan 2014
+%%%-------------------------------------------------------------------
+
 -module(bookstore).
 -export([server/1, client/3, start/0]).
+
 server(State) -> 
     receive 
 	{add, Book, Author, Price, Qty} ->
@@ -7,32 +14,28 @@ server(State) ->
 		false -> StateNew = [{Book, Author,Price, Qty}|State],
 			 io:fwrite("~p has been added to our inventory ~n", [Book]),
 			 server(StateNew);
-		_-> server(State) %% shouldn't we add quantity here?
+		_-> server(State) %% it makes sense to add quantity here but it's specified that we don't do anything.
 	    end;
 	{buy, Book, Pid} ->
 	    case lists:keysearch(Book,1, State) of
-		false -> io:fwrite("buy request from ~p ~n Sorry ~p, we don't have ~p",[Pid,Pid, Book]),
+		false -> io:fwrite("buy request recieved from ~p ~n Sorry ~p, we don't have ~p",[Pid,Pid, Book]),
 			 server(State);
 		{_,Tuple} -> case element(4,Tuple) of
-				 0 -> io:fwrite("buy request from ~p ~n Sorry ~p, ~p is sold out",[Pid,Pid, Book]),
+				 0 -> io:fwrite("buy request recieved from ~p ~n Sorry ~p, ~p is sold out",[Pid,Pid, Book]),
 				      server(State);
 				 _ -> StateNew = lists:keyreplace(Book, 1, State,setelement(4,Tuple, element(4,Tuple) -1)),
-				      io:fwrite("buy request from ~p ~n Client ~p bought a copy of ~p", [Pid,Pid,Book]),
+				      io:fwrite("buy request recieved from ~p ~n Client ~p bought a copy of ~p~n", [Pid,Pid,Book]),
 				      io:format("new inventory: "),
 				      lists:map(fun(X) -> io:format("~p~n",[X]) end, StateNew),
 				      server(StateNew)
 			     end
 	    end;
-	{close} -> io:format("closed for today!"),
+	close -> io:format("closed for today!~n"),
 		   exit(normal);
-	_ -> io:fwrite("What is this witch-craft?!"),
+	_ -> io:fwrite("What is this witch-craft?!~n"),
 	     server(State)
 		       
     end.
-
-	
-    %Bookus = whereis(bookus),
-    %io:fwrite("~p ~n", [Bookus]). 
 
 client(Server , X, Wait) ->
     timer:sleep(Wait),
@@ -41,6 +44,7 @@ client(Server , X, Wait) ->
 	  2 -> Server ! {buy,"Excession",self()};
 	  3 -> Server ! {buy,"The Forever War",self()}
 end.
+
 start() -> 
     Server = spawn(bookstore , server, [[]]),
     %% Server ! {add, Name, Author, Price, Quantity}
